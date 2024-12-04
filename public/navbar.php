972918dbaade607c59c805b30d66123dc2104b60
Yvanovitch <RasamimananaJason@gmail.com>
2024-12-04 18:00:29 +0100

    a

--- public/navbar.php
+++ public/navbar.php
@@ -1,18 +1,42 @@
 <?php
     include __DIR__. '/session.php';
 ?>
-<nav>
-    <div>
-        <ul>
-            <li>Hotel Neptune</li>
-            <li>Accueil</li>
-            <li>Chambre</li>
-            <li>A propos</li>
-            <li>Contact</li>
-        </ul>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Hotel Neptune</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">A propos</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Chambre
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Familial</a></li>
+            <li><a class="dropdown-item" href="#">Quelque chose</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
+  </div>
 </nav>
-
 
 <nav>
     <?php if(!$isLoggedIn) {?>
--- public/register.php
+++ public/register.php
@@ -54,18 +54,29 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
     <?php include __DIR__. '/navbar.php' ?>
     <div class="container">
-        <form action="" method="post">
-            <input type="text" name="firstname" placeholder="Nom" required>
-            <input type="text" name="lastname" placeholder="Prénom" required>
-            <input type="email" name="email" placeholder="Email" required>
-            <input type="password" name="password" placeholder="Mot de Passe" required>
-            <input type="submit" value="s'inscrire">
-        </form>
+        <div class="card  border-light mb-3" style="max-width: 18rem;">
+            <form action="" method="post">
+                <input type="text" name="firstname" placeholder="Nom" required>
+                <input type="text" name="lastname" placeholder="Prénom" required>
+                <input type="email" name="email" placeholder="Email" required>
+                <input type="password" name="password" placeholder="Mot de Passe" required>
+                <input type="submit" value="s'inscrire" class="btn btn-primary">
+            </form>
+        </div>
     </div>
+    <div class="card>
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h5 class="card-title">Light card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+
 </body>
 </html>
