e8f416575910e855911a078e76f71cdf6a983df4
Yvanovitch <RasamimananaJason@gmail.com>
2024-12-04 15:04:53 +0100

    a

--- public/register.php
+++ public/register.php
@@ -1,3 +1,41 @@
+<?php
+
+require_once __DIR__.'/../database.php';
+
+if($_SERVER('REQUEST_METHOD') === 'POST'){
+    if(!isset($_POST['firstname'])
+    || !isset($_POST['lastname'])
+    || !isset($_POST['email'])
+    || !isset($_POST['password'])
+    ){
+        $message='Il manque une information.';
+        header('Location: /error.php?message= '. urlencode($message));
+        exit;
+    }
+}
+
+    if($_SERVER('REQUEST_METHOD') === 'POST'){
+        if(empty($_POST['firstname'])
+        || empty($_POST['lastname'])
+        || empty($_POST['email'])
+        || empty($_POST['password'])
+        ){
+            $message='Tous les chamsp doivent être renseignés.';
+            header('Location: /error.php?message= '. urlencode($message));
+            exit;
+        }
+        
+        $query = $pdo -> prepare('SELECT * FROM users WHERE users.email = :email');
+        $query = $query->execute(['email' => $_POST ['email']]);
+
+        if($query->fetch{}){
+            $message='Il utilisateur exist déja utilisateur du même nom.';
+            header('Location: /error.php?message= '. urlencode($message));
+            exit;
+        }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +45,13 @@
 </head>
 <body>
     <div class="container">
-        <form action=""></form>
+        <form action="" method="post">
+            <input type="text" name="firstname" placeholder="Nom" required>
+            <input type="text" name="lastname" placeholder="Prénom" required>
+            <input type="email" name="email" placeholder="Email" required>
+            <input type="password" name="password" placeholder="Mot de Passe" required>
+            <input type="submit" value="s'inscrire">
+        </form>
     </div>
 </body>
 </html>
