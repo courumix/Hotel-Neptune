8584277e0d5c897b2f7571c85ca13b32369a9930
fatouth <fatouthiobane04@gmail.com>
2024-12-04 17:35:06 +0100

    Update navbar.php

--- public/navbar.php
+++ public/navbar.php
@@ -1,21 +1,24 @@
 <?php
-    include __DIR__. '/../session.php';
+    include __DIR__. '/session.php';
 ?>
 
 <nav>
-    <?php if(!$isLoggedIn) {?>
+    <?php if(!$isLoggedIn) {}
+        ?>
         <li class="nav-item">
             <a href="nav-link" href="/public/login.php">Connexion</a>
         </li>
         <li class="nav-item">
             <a href="/public/register.php" class="nav-link">Inscript</a>
         </li>
-    <?php} else {?>
+    
+    <?php else {?>
         <li class="nav-item">
             <a href="" class="nav-link"></a>
         </li>
         <li class="nav-item">
             <a href="" class="nav-link"></a>
         </li>
-    <?php}?>
+    <?php} 
+    ?>
 </nav>
