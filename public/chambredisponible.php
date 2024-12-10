47ad95a9071b4415c83384043911efa5b525962b
Yvanovitch <RasamimananaJason@gmail.com>
2024-12-10 17:43:08 +0100

    logo pour tout

--- public/chambredisponible.php
+++ public/chambredisponible.php
@@ -10,6 +10,7 @@ require_once __DIR__ . '/session.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chambres Disponibles</title>
+    <link rel="icon" href="/Photo/Hotel-Neptune-logo.jpg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
--- public/listechambre.php
+++ public/listechambre.php
@@ -11,6 +11,7 @@ require_once __DIR__ . '/session.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
+    <link rel="icon" href="/Photo/Hotel-Neptune-logo.jpg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Liste des chambres</title>
 </head>
