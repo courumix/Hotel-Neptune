1fe80692d81e68b29268dccca6b2603183b18839
Yvanovitch <RasamimananaJason@gmail.com>
2024-12-10 15:14:24 +0100

    sujet

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@ require_once __DIR__. '/session.php';
     <?php include __DIR__. '/navbar.php' ?>
     <h1>Hotel Neptune</h1>
     <?php echo $isLoggedIn ? 'Je suis connecter' : 'Je suis pas connecter'; ?>
-    <img src="https://hapi.mmcreation.com/hapidam/4441d8ee-41d6-4bb8-93ac-a5834b6bd111/Facade_2_by_Night_-_CHotel_Raphael_Paris.jpg.jpg?size=lg" alt="">
+    <img src="https://hapi.mmcreation.com/hapidam/4441d8ee-41d6-4bb8-93ac-a5834b6bd111/Facade_2_by_Night_-_CHotel_Raphael_Paris.jpg.jpg?size=lg" class="img-fluid" alt="...">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
