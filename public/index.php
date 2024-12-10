541f8a5b09f9b5f1706632497abeda60a3f5668e
Courumix <71878634+courumix@users.noreply.github.com>
2024-12-10 14:06:02 +0100

    d

--- public/index.php
+++ public/index.php
@@ -20,9 +20,11 @@ require_once __DIR__. '/../session.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <<<<<<< HEAD
     <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Hotel Neptune</title>
 </head>
 <body>
+    <?php include __DIR__ . '/navbar.php'; ?>
     <h1>Hello <?php echo 'World'; ?></h1>
     <a href="connexion.php"><h1>connexion</h1></a>
     <a href="inscription.php"><h1>inscription</h1></a>
--- public/navbar.php
+++ public/navbar.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
     include __DIR__. '/session.php';
 ?>
@@ -21,4 +22,54 @@
         </li>
     <?php} 
     ?>
+=======
+<h?php
+
+require_once __DIR__ . '/session.php';
+
+?>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Hotel Neptune</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <?php if(!$isLoggedIn){ ?>
+      <a href="connexion.php"><button type="button" class="btn btn-primary btn-sm me-2">Connexion</button></a>
+      <a href="inscription.php"><button type="button" class="btn btn-secondary btn-sm">Inscription</button></a>
+      <?php } else { ?>
+      <div>
+        <h3><span class="badge text-bg-primary">
+        <?php echo $_SESSION['user']['firstname'] . ' ' . $_SESSION['user']['lastname']; ?>
+      </span></h3>
+      <a href="déconnexion.php"><button type="button" class="btn btn-danger">Deconnexion</button></a>
+      </div>
+      <?php } ?>
+    </div>
+  </div>
+>>>>>>> Stashed changes
 </nav>
