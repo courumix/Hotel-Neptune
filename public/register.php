f1edb8932344ae81fdab395c67f028157a38f94c
Yvanovitch <RasamimananaJason@gmail.com>
2024-12-10 15:02:38 +0100

    a

--- public/register.php
+++ public/register.php
@@ -70,13 +70,5 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
             </form>
         </div>
     </div>
-    <div class="card>
-  <div class="card-header">Header</div>
-  <div class="card-body">
-    <h5 class="card-title">Light card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-
 </body>
 </html>
