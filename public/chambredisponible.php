def4b75ea8bf3716f527f22a5fbb809bc7e15495
Yvanovitch <RasamimananaJason@gmail.com>
2024-12-10 16:07:48 +0100

    Create chambredisponible.php

---
+++ public/chambredisponible.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chambre</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <?php include __DIR__. '/navbar.php';?>
+    <h2>Chambre Disponible</h2>
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://www.yonder.fr/sites/default/files/styles/lg-insert/public/contenu/destinations/hotel%20richer%20de%20belleval%202%C2%A9%20jerome%20mondiere.jpg?itok=pIlrrovQ" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Chambre Premium</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://static-otelico.com/cache/regina_berck/hotel_regina-berck_sur_mer-chambre_standard_6.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Chambre Premium</h5>
+        <p class="card-text">This is a short card.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://www.mews.com/hs-fs/hubfs/type%20de%20chambre%20par%20agencement.webp?width=800&height=600&name=type%20de%20chambre%20par%20agencement.webp" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Chambre Premium</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://www.hoteldefrance-paris.fr/cache/img/1956b1562896f1b006a01221b84eaa7e6a30df55-1956b1-3840-2160-auto.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Chambre Premium</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://www.hoteldesprinces.com/wp-content/uploads/2024/05/20240530_160716-scaled.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Chambre Familiale</h5>
+        <p class="card-text">Idéale pour les familles nombreuses, l’Hôtel des Princes Chambéry vous propose 2 chambres familiales pouvant accueillir 4 adultes et 2 enfants. Disposant de 2 lits doubles (150cm) et de 2 lits superposés (90cm), ces 2 chambres familiales bénéficient d’une vue imprenable sur la rue de Boigne, le monument des Eléphants et le château des Ducs de Savoie. Vous profitez d’un espace pour passer vos vacances entre amis ou en famille.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://static-otelico.com/cache/lacouronne/hotel_marmande_la_couronne_9_2.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Chambre Familiale A partir de <b>110,00 €</b> Prix indicatif, hors promotions et saisonnalité</h5>
+        <p class="card-text">Par sa grande taille cette chambre peut accueillir 5 personnes, son tarif varie en fonction de l'occupation :
+<br>3 personnes :à partir de 90€
+<br>4 personnes : à partir de 110€
+<br>5 personnes : à partir de 120€
+<br>Profitez aussi de nos petits déjeuners pour 9.80 € par personne servi de 7h00 à 10h00 du lundi au vendredi et de 8h00 à 10h00 le samedi et dimanche.
+<br>
+Cette chambre adaptée est réservée pour les personnes à mobilité réduite ou pour une occupation supérieure à 3 personnes (groupes et familles).</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://hotel-montbriand-antony.com/wp-content/uploads/2020/07/chambre-familiale.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Chambre Familiale</h5>
+        <p class="card-text">Superficie : 35 m²
+<br>Climatisation
+<br>Situé au 4ème étage, côté rue
+<br>Chambre non fumeur
+<br>Un grand lit double 160×190 et un canapé-lit pour 2 enfants/adolescents
+<br>Bureau
+<br>Wifi haut débit gratuit et illimité
+<br>Télévision à écran plat avec chaînes satellite (Canal+)
+<br>Mini-bar (avec boissons et snack, non-inclus dans le tarif de la chambre)
+<br>Coffre-fort
+<br>Salle de bain avec baignoire, sèche-cheveux
+<br>WC séparé de la salle de bain
+<br>Produits d’accueil salle de bain : Shampoing – Gel douche – Savon 20g
+<br>Plateau de courtoisie (thé et café) et bouilloire
+<br>1 animal de compagnie accepté par chambre : Chien/chat inférieure à 15kg (supplément 15€ par jour)
+<br>Lit parapluie – jusqu’à 3 ans – gratuit – maximum 1 par chambre : sur demande à l’avance et sous réserve de disponibilité
+<br>Jeux d’intérieurs pour jeunes enfants et adolescents disponible sur demande
+<br>Équipement pour bébé disponible sur demande
+<br>Téléphone</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://www.hotelmistralcomedie.com/public/img/big/CHAMBREFAMILIAL10jpeg_620e64ba75304.jpeg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Chambre Familiale</h5>
+        <p class="card-text">Nos Chambres FAMILIALES sont idéales pour un séjour en famille à Montpellier. Conçues pour 2 adultes et 2 enfants, nos chambres familiales disposent d’un lit double (160x200), et d’un lit superposé pour le bonheur des plus jeunes, le tout sur une belle surface de 18,5 m².
+<br>Elles ont, bien sûr, une salle de bain privative avec douche et toilettes séparées. Situées en étage, elles possèdent toutes un balcon avec une vue sur la rue Boussairolles, la rue calme de l’hôtel.
+<br>Les chambres Chambres FAMILIALES  sont équipées d’une connexion WiFi gratuite, d’une télévision écran plat avec les chaînes de la TNT, d’une climatisation réversible, d’une armoire avec penderie et d’un bureau. Cette chambre est idéale pour passer un week-end en famille à Montpellier.</p>
+      </div>
+    </div>
+  </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
